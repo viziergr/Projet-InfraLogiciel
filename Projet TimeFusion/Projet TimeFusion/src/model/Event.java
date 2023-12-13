502de2a81d5992d7577fc312a7ca29179c5c2e7d
LouisFlop <louis.peculier@reseau.eseo.fr>
2023-12-13 14:55:33 +0100

    pushed

--- Projet TimeFusion/Projet TimeFusion/src/model/Event.java
+++ Projet TimeFusion/Projet TimeFusion/src/model/Event.java
@@ -13,8 +13,85 @@ public class Event {
 
     // Constructeurs, getters, setters
 
-    public Event() {
+    public Event(Long id, String title, LocalDateTime startTime, LocalDateTime endTime, String location, String description, Boolean isPersonal) {
+        this.id = id;
+        this.title = title;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.location = location;
+        this.description = description;
+        this.isPersonal = isPersonal;
     }
 
     // autres constructeurs, getters et setters...
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle(){
+        return this.title;
+    }
+
+    public void setTitle(String title){
+        this.title = title;
+    }
+
+    public LocalDateTime getStartTime(){
+        return this.startTime;
+    }
+
+    public void setStartTime(LocalDateTime startTime){
+        this.startTime = startTime;
+    }
+
+    public LocalDateTime getEndTime(){
+        return this.endTime;
+    }
+
+    public void setEndTime(LocalDateTime endTime){
+        this.endTime = endTime;
+    }
+
+    public String getLocation(){
+        return this.location;
+    }
+
+    public void setLocation(String location){
+        this.location = location;
+    }
+
+    public String getDescription(){
+        return this.description;
+    }
+
+    public void setDescription(String description){
+        this.description = description;
+    }
+
+    public Boolean getIsPersonal(){
+        return this.isPersonal;
+    }
+
+    public void setIsPersonal(Boolean isPersonal){
+        this.isPersonal = isPersonal;
+    }
+
+    @Override
+    public String toString() {
+        return "Event{" +
+                "id=" + id +
+                ", title='" + title +
+                ", startTime='" + startTime +
+                ", endTime='" + endTime +
+                ", location='" + location +
+                ", description='" + description +
+                ", isPersonal='" + isPersonal +
+                '}';
+    }
+    
 }
