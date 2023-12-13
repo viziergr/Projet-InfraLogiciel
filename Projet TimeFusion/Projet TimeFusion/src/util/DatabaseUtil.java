9ed17e5d7797e34c8d728b3f1ad878eb5da85946
LouisFlop <louis.peculier@reseau.eseo.fr>
2023-12-13 15:14:16 +0100

    pushed

--- Projet TimeFusion/Projet TimeFusion/src/util/DatabaseUtil.java
+++ Projet TimeFusion/Projet TimeFusion/src/util/DatabaseUtil.java
@@ -1,5 +1,18 @@
 package util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DatabaseUtil {
-    
+
+    private static final String DATABASE_URL = "jdbc:your_database_url";
+    private static final String DATABASE_USER = "your_username";
+    private static final String DATABASE_PASSWORD = "your_password";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(DATABASE_URL, DATABASE_USER, DATABASE_PASSWORD);
+    }
+
+    // You can add other database-related utility methods here
 }
--- Projet TimeFusion/Projet TimeFusion/src/util/Validationutil.java
+++ Projet TimeFusion/Projet TimeFusion/src/util/Validationutil.java
@@ -1,5 +1,20 @@
 package util;
 
-public class Validationutil {
-    
+public class ValidationUtil {
+
+    public static boolean isValidEmail(String email) {
+        String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\." +
+                            "[a-zA-Z0-9_+&*-]+)*@" +
+                            "(?:[a-zA-Z0-9-]+\\.)+[a-z" +
+                            "A-Z]{2,7}$";
+        return email != null && email.matches(emailRegex);
+    }
+
+    public static boolean isValidPassword(String password) {
+        // Implement your password policy here
+        return password != null && password.length() >= 8;
+    }
+
+    // Additional validation methods can be added here
 }
+
