14bc8d7a17e00a16f3016e9286875769660ba321
CorentinRbn <corentin.robin@reseau.eseo.fr>
2023-12-20 22:00:19 +0100

    Add JDBC driver loading code to DatabaseUtil.java

--- Projet TimeFusion/Projet TimeFusion/ApplicationBureau/src/util/DatabaseUtil.java
+++ Projet TimeFusion/Projet TimeFusion/ApplicationBureau/src/util/DatabaseUtil.java
@@ -366,4 +366,15 @@ public class DatabaseUtil {
       return statement.executeUpdate();
     }
   }
+
+  public static void main(String[] args) {
+    try {
+      // Load the JDBC driver
+      Class.forName("com.mysql.cj.jdbc.Driver");
+
+      System.out.println("MySQL JDBC driver loaded successfully.");
+    } catch (ClassNotFoundException e) {
+      e.printStackTrace(); // Handle the exception appropriately
+    }
+  }
 }
