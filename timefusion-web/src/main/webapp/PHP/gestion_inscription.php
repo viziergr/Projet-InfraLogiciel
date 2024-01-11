c2162363580b8e0755e976e513c27838a87be798
Grégoire VIZIER <gregoire.vizier@reseau.eseo.fr>
2024-01-11 14:13:29 +0100

    Ajout de year

--- timefusion-web/src/main/webapp/PHP/gestion_inscription.php
+++ timefusion-web/src/main/webapp/PHP/gestion_inscription.php
@@ -35,7 +35,7 @@ if (isset($_POST['inscription_submit']) && $_POST['inscription_submit'] == 2) {
                 while (!$id_defined){
                     try {
                         $id = rand();
-                        $sql = "INSERT INTO `User` (id, first_name, last_name, email, password) VALUES ('$id','$nom', '$prenom', '$email', '$pwd')";
+                        $sql = "INSERT INTO `User` (id, first_name, last_name, email, password, year) VALUES ('$id','$nom', '$prenom', '$email', '$pwd', '$annee')";
                         $mysqli->query($sql);
                         $id_defined = true;
                         echo "Inscrit";
