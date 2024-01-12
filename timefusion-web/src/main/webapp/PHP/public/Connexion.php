24a87ae68490635d41d324968e153ea76f28bf97
CorentinFlop <corentin.grosset@reseau.eseo.fr>
2024-01-12 10:19:38 +0100

    modified connexion.php

--- timefusion-web/src/main/webapp/PHP/public/Connexion.php
+++ timefusion-web/src/main/webapp/PHP/public/Connexion.php
@@ -8,30 +8,38 @@
     <link rel="stylesheet" href="..\..\CSS\code.css">
     <meta charset="utf-8">
 </head>
-<body class="cnx">
-    <div class="home">
+<body>
+
+    <div class="bande">
         <a href="..\..\HTML\index.html">
             <img id="home" src="..\..\pictures\home.png" alt="Accueil">
         </a>
     </div>
+
     <?php if (empty($_SESSION['compte'])): ?>
-        <h1 id="bvn1">Bienvenue</h1>
-        <img id="img2" src="..\..\pictures\Logo.png" alt="Logo Time Fusion">
+    <div class="cnx">
+        <img src="..\..\pictures\Logo.png" alt="Logo Time Fusion">    
+        <h1>Bienvenue</h1>
         <form method="post">
             <div>          
-                <input type="email" id="email1" name="mail" placeholder="E-mail" required>
+                <input type="email" id="email" name="mail" placeholder="E-mail" required>
             </div>
             <div>
-                <input type="password" id="password1" name="password" placeholder="Mot de passe" required>
+                <input type="password" id="password" name="password" placeholder="Mot de passe" required>
             </div>
             <div>
-                <button type="submit" id="boutonBleu1" value="1" name="connexion_submit">Connexion</button>
+                <button type="submit" id="boutonConnexion" value="1" name="connexion_submit">Connexion</button>
             </div>
         </form>
-        <p id="pdc">Pas de compte ?</p>
+    </div>
+
+    <div class = "redirectionInscription">  
+        <p>Pas de compte ?</p>
         <a href="..\..\PHP\Inscription.php">
-            <button id="btnRed1" type="button">Inscrivez vous</button>
+            <button type="button">Inscrivez vous</button>
         </a>
+    </div>
+
     <?php endif; ?>
 </body>
 
