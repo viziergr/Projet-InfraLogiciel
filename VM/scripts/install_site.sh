20699911efe4b0384901e42666efdcb55a641cab
viziergr <gregoire.vizier@gmail.com>
2024-01-12 09:43:38 +0100

    commit

--- VM/scripts/install_site.sh
+++ VM/scripts/install_site.sh
@@ -26,13 +26,11 @@ rm /var/www/html/git/Projet-InfraLogiciel/ProjetInfraLog.drawio
 rm /var/www/html/git/Projet-InfraLogiciel/README.md
 
 cp -r /var/www/html/git/Projet-InfraLogiciel/timefusion-web/src/main/webapp/* /var/www/html/siteweb/
-cp -r /var/www/html/siteweb/PHP/public/* /var/www/html/siteweb/public/
-cp -r /var/www/html/siteweb/public/index.html /var/www/html/siteweb/index.html
 mkdir /var/www/html/siteweb/myadmin
 mv /var/www/html/myadmin/* /var/www/html/siteweb/myadmin/
 
 # modification de la configuration du site 000-default.conf pour pointer sur le dossier siteweb
-sed -i 's/DocumentRoot \/var\/www\/html/DocumentRoot \/var\/www\/html\/siteweb/g' /etc/apache2/sites-available/000-default.conf
+sed -i 's/DocumentRoot \/var\/www\/html/DocumentRoot \/var\/www\/html\/siteweb/PHP/public/g' /etc/apache2/sites-available/000-default.conf
 
 service apache2 reload
 echo "END - Deplacement des fichiers"
