f4b3118d1100da851fe4898ee72e18861da81362
viziergr <gregoire.vizier@gmail.com>
2024-01-12 10:29:29 +0100

    Commit

--- VM/scripts/install_site.sh
+++ VM/scripts/install_site.sh
@@ -28,7 +28,8 @@ rm /var/www/html/git/Projet-InfraLogiciel/README.md
 cp -r /var/www/html/git/Projet-InfraLogiciel/timefusion-web/src/main/webapp/* /var/www/html/siteweb/
 mkdir /var/www/html/siteweb/myadmin
 mv /var/www/html/myadmin/* /var/www/html/siteweb/myadmin/
-cp -r /var/www/html/siteweb/CSS/* /var/www/html/siteweb/PHP/public/
+mkdir /var/www/html/siteweb/PHP/public/CSS
+cp -r /var/www/html/siteweb/CSS/* /var/www/html/siteweb/PHP/public/CSS
 # modification de la configuration du site 000-default.conf pour pointer sur le dossier siteweb/PHP/public
 sed -i 's/\/var\/www\/html/\/var\/www\/html\/siteweb\/PHP\/public/g' /etc/apache2/sites-available/000-default.conf
 
