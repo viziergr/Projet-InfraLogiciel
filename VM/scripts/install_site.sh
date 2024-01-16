c531fbeeb6cf8d04c9c83ba05cd66329bc13aad8
viziergr <gregoire.vizier@gmail.com>
2024-01-16 08:42:51 +0100

    Update installation script and remove unnecessary files

--- VM/scripts/install_site.sh
+++ VM/scripts/install_site.sh
@@ -17,13 +17,19 @@ if [ -d "$repertoire" ]; then
     # Le répertoire existe, exécuter git pull
     cd "$repertoire" || exit
     git pull
+    # Modification de la configuration du site 000-default.conf pour pointer sur le dossier siteweb/PHP/public
+    sed -i 's/\/var\/www\/html/\/var\/www\/html\/Projet-Infralogiciel\/timefusion-web\/public/g' /etc/apache2/sites-available/000-default.conf
+    
+    # Déplacement des fichiers de myadmin
+    mkdir /var/www/html/Projet-InfraLogiciel/timefusion-web/myadmin
+    mv /var/www/html/myadmin/* /var/www/html/Projet-InfraLogiciel/timefusion-web/myadmin/
 else
     # Le répertoire n'existe pas, exécuter git clone
     git clone "https://github.com/viziergr/Projet-InfraLogiciel.git" "$repertoire"
     git checkout gregoire/testArchitecture
 fi
 
-
+# Suppression de tous les autres fichiers inutiles
 rm -r /var/www/html/Projet-InfraLogiciel/.vscode
 rm -r /var/www/html/Projet-InfraLogiciel/livrables
 rm -r /var/www/html/Projet-InfraLogiciel/timefusion-dekstop
@@ -31,12 +37,6 @@ rm -r /var/www/html/Projet-InfraLogiciel/VM
 rm /var/www/html/Projet-InfraLogiciel/Configurations.txt
 rm /var/www/html/Projet-InfraLogiciel/ProjetInfraLog.drawio
 rm /var/www/html/Projet-InfraLogiciel/README.md
-
-mkdir /var/www/html/Projet-InfraLogiciel/timefusion-web/myadmin
-mv /var/www/html/myadmin/* /var/www/html/Projet-InfraLogiciel/timefusion-web/myadmin/
-
-# modification de la configuration du site 000-default.conf pour pointer sur le dossier siteweb/PHP/public
-sed -i 's/\/var\/www\/html/\/var\/www\/html\/Projet-Infralogiciel\/timefusion-web\/public/g' /etc/apache2/sites-available/000-default.conf
 
 service apache2 reload
 echo "END - Deplacement des fichiers"
