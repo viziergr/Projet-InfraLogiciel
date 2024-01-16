6cf642a7bb5e5aa94cb067d49d243db86f2f5e0f
viziergr <gregoire.vizier@gmail.com>
2024-01-16 08:58:35 +0100

    Update installation script and remove unnecessary files

--- VM/scripts/install_site.sh
+++ VM/scripts/install_site.sh
@@ -14,21 +14,27 @@ repertoire="/var/www/html/Projet-InfraLogiciel"
 
 # Vérifier si le répertoire existe
 if [ -d "$repertoire" ]; then
+    echo "[1] - Le répertoire existe"
     # Le répertoire existe, exécuter git pull
     cd "$repertoire" || exit
+    echo "[2] - Git pull"
     git pull
+    echo "[3] - Git Modification de la configuration du site 000-default.conf"
     # Modification de la configuration du site 000-default.conf pour pointer sur le dossier siteweb/PHP/public
     sed -i 's/\/var\/www\/html/\/var\/www\/html\/Projet-Infralogiciel\/timefusion-web\/public/g' /etc/apache2/sites-available/000-default.conf
-    
+    echo "[4] - Déplacement du répertoire myadmin"
     # Déplacement des fichiers de myadmin
     mkdir /var/www/html/Projet-InfraLogiciel/timefusion-web/myadmin
     mv /var/www/html/myadmin/* /var/www/html/Projet-InfraLogiciel/timefusion-web/myadmin/
 else
     # Le répertoire n'existe pas, exécuter git clone
+    echo "[1] - Git clone"
     git clone "https://github.com/viziergr/Projet-InfraLogiciel.git" "$repertoire"
+    echo "[2] - Git checkout gregoire/testArchitecture"
     git checkout gregoire/testArchitecture
 fi
 
+echo "START - Suppression des fichiers inutiles"
 # Suppression de tous les autres fichiers inutiles
 rm -r /var/www/html/Projet-InfraLogiciel/.vscode
 rm -r /var/www/html/Projet-InfraLogiciel/livrables
@@ -37,6 +43,7 @@ rm -r /var/www/html/Projet-InfraLogiciel/VM
 rm /var/www/html/Projet-InfraLogiciel/Configurations.txt
 rm /var/www/html/Projet-InfraLogiciel/ProjetInfraLog.drawio
 rm /var/www/html/Projet-InfraLogiciel/README.md
+echo "END - Suppression des fichiers inutiles"
 
 service apache2 reload
 echo "END - Deplacement des fichiers"
