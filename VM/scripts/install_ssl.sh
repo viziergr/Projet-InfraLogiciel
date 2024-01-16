4e8082803947d94632d19e3cf7beaa03723010a7
viziergr <gregoire.vizier@gmail.com>
2024-01-16 10:50:32 +0100

    Update SSL installation script

--- VM/scripts/install_ssl.sh
+++ VM/scripts/install_ssl.sh
@@ -9,17 +9,20 @@ LOG_FILE="/vagrant/logs/install_git.log"
 DEBIAN_FRONTEND="noninteractive"
 
 echo "START - Installation du certificat SSL - "$IP
+
 echo "=> [1] - Installation de snapd"
-sudo apt install snapd
-echo "=> [2] - Installation de snap core"
-sudo snap install core
-echo "=> [3] - Suppression de certbot"
+apt-get install $APT_OPT \
+  snapd \
+  core \
+  >> $LOG_FILE 2>&1
+
+echo "=> [2] - Suppression de certbot"
 sudo apt-get remove certbot
-echo "=> [4] - Installation de certbot"
+echo "=> [3] - Installation de certbot"
 sudo snap install --classic certbot
-echo "=> [5] - Préparation de la commande Certbot"
+echo "=> [4] - Préparation de la commande Certbot"
 sudo ln -s /snap/bin/certbot /usr/bin/certbot
-echo "=> [6] - Installation du certificat SSL"
+echo "=> [5] - Installation du certificat SSL"
 sudo certbot --apache --register-unsafely-without-email --agree-tos
 
 echo "END - Installation du certificat SSL - "$IP
