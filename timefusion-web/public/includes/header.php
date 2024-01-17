efbd8c47bf64ffa01d3c884ba95be32dcea8b16b
viziergr <gregoire.vizier@gmail.com>
2024-01-17 19:58:45 +0100

    Update calendar.css file path in header.php

--- timefusion-web/public/includes/header.php
+++ timefusion-web/public/includes/header.php
@@ -4,7 +4,7 @@
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href= "../../assets/CSS/calendar.css">
+    <link rel="stylesheet" href= "../../../assets/CSS/calendar.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 <body>
