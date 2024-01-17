3bbe75a8ca2f5a200edbc5562d4c30d494d16b54
viziergr <gregoire.vizier@gmail.com>
2024-01-17 20:24:36 +0100

    Update logout redirect path

--- timefusion-web/public/scripts/gestion_deconnexion.php
+++ timefusion-web/public/scripts/gestion_deconnexion.php
@@ -1,7 +1,7 @@
 <?php
 if (isset($_GET['logout']) && $_GET['logout'] == 1) {
     unset($_SESSION['compte']);
-    header("Location: ../index.html");
+    header("Location: ../../index.html");
 }
 
 ?>
