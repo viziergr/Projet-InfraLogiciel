ea42b8f09361baaaba782bdb05ff26a55c4466e1
viziergr <gregoire.vizier@gmail.com>
2024-01-17 18:47:37 +0100

    Update file paths in Calendrier.php

--- timefusion-web/public/pages/needLog/Calendrier.php
+++ timefusion-web/public/pages/needLog/Calendrier.php
@@ -1,9 +1,9 @@
 <?php 
 
-require __DIR__ .'\..\..\src\bootstrap.php';
-require __DIR__ .'\..\..\src\Calendar\Month.php';
-require __DIR__ .'\..\..\src\Calendar\Events.php'; 
-require __DIR__ .'\..\..\src\Team\Teams.php';   
+require __DIR__ .'\..\..\scripts\bootstrap.php';
+require __DIR__ .'\..\..\scripts\Calendar\Month.php';
+require __DIR__ .'\..\..\scripts\Calendar\Events.php'; 
+require __DIR__ .'\..\..\scripts\Team\Teams.php';   
 
 sess_exists();
 
