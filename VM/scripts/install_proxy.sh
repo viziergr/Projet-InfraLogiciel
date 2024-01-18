b7837dffe174d16faa11506d3d3626e3e30c78cb
viziergr <gregoire.vizier@gmail.com>
2024-01-18 16:12:54 +0100

    Add reverse proxy configuration to install_proxy.sh script

--- VM/scripts/install_proxy.sh
+++ VM/scripts/install_proxy.sh
@@ -1,36 +1,35 @@
 #!/bin/bash
 
-## déplace les fichiers du site
+LOG_FILE="/vagrant/logs/install_proxy.log"
+# Ajout du reverse proxy dans le fichier ci-dessous qui permet d'accéder au site
+echo "start - install proxy"
+sudo sed -i 's/<\/VirtualHost>//g' /etc/apache2/sites-available/000-default.conf
+echo "ServerName .reverse-proxy
+        # Activation du module de proxy
+        ProxyRequests Off
+        ProxyPreserveHost On
 
-IP=$(hostname -I | awk '{print $2}')
+        <Proxy *>
+         Order deny,allow
+         Allow from all
+        </Proxy>
 
-APT_OPT="-o Dpkg::Progress-Fancy="0" -q -y"
-LOG_FILE="/vagrant/logs/install_git.log"
-DEBIAN_FRONTEND="noninteractive"
+        # Configuration du reverse proxy
+        ProxyPass / http://192.168.56.80/
+        ProxyPassReverse / http://192.168.56.80/
+</VirtualHost>" >> /etc/apache2/sites-available/000-default.conf
 
-echo "START - Installation reverse proxy - "$IP
+# Activation des modules proxy et proxy_http sur le serveur web Apache
+a2enmod proxy proxy_http
 
-sudo a2enmod proxy
-sudo a2enmod proxy_http
+# Création du nouvel hôte pour le reverse proxy
+sudo mkdir -p /etc/apache2/sites-available 
+sudo touch /etc/apache2/sites-available/reverse-proxy.conf
 
-sudo systemctl restart apache2
+# Activation du reverse proxy
+sudo a2ensite reverse-proxy 
 
-echo "<VirtualHost *:80>
-    ProxyRequests Off
-    ProxyPreserveHost On
-
-    <Proxy *>
-        Order deny,allow
-        Allow from all
-    </Proxy>
-
-    ProxyPass / http://192.168.56.80/
-    ProxyPassReverse / http://192.168.56.80/
-
-</VirtualHost>">/etc/apache2/sites-available/001-reverse-proxy.conf
-sudo a2dissite 000-default.conf
-sudo a2ensite 001-reverse-proxy.conf
-sudo systemctl reload apache2
-sudo service apache2 restart
-
-echo "END - Installation reverse proxy"
+# Relancer apache2 pour mettre à jour le proxy
+sudo systemctl reload apache2 
+sudo service apache2 restart 
+echo "END - reverse proxy installed"
