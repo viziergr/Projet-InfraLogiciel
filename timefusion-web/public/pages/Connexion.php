0f8d945936a7c6ad1f5fb0bf9d5604c8500eb1a0
LouisFlop <louis.peculier@reseau.eseo.fr>
2024-01-21 11:53:00 +0100

    pushed

--- timefusion-web/public/pages/Connexion.php
+++ timefusion-web/public/pages/Connexion.php
@@ -5,20 +5,29 @@
 <html>
 <head>
     <title>Formulaire de connexion</title>
-    <link rel="stylesheet" href="/assets/CSS/code.css">
+    <link rel="stylesheet" href="../assets/CSS/code.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <meta charset="utf-8">
 </head>
 <body>
+
     <div class="bande">
-        <a href="index.html">
-        <img id="home" src="/assets/pictures/home.png" alt="Accueil">
+        <a href="../index.html">
+            <img id="home" src="../assets/pictures/home.png" alt="Accueil">
         </a>
     </div>
-    
+
     <?php if (empty($_SESSION['compte'])): ?>
     <div class="cnx">
-        <img src="/assets/pictures/Logo.png" alt="Logo Time Fusion">
+        <img src="../assets/pictures/Logo.png" alt="Logo Time Fusion">    
         <h1>Bienvenue</h1>
+        <?php if (isset($error)): ?>
+            <div class="container">
+                <div class="alert alert-danger">
+                    <?= $error ?>
+                </div>
+            </div>
+        <?php endif; ?>
         <form method="post">
             <div>          
                 <input type="email" id="email" name="mail" placeholder="E-mail" required>
@@ -30,21 +39,22 @@
                 <button type="submit" id="boutonConnexion" value="1" name="connexion_submit">Connexion</button>
             </div>
         </form>
-    </div>
+    </div>    
 
-    <div class="redirectionInscription">
+    <div class = "redirectionInscription">  
         <p>Pas de compte ?</p>
         <a href="Inscription.php">
             <button type="button">Inscrivez vous</button>
         </a>
     </div>
-
     <div class="oubliMDP">
         <a href="forgetPassword.php">
             <button id="newMDP" type="button">Mot de passe oublié ?</button>
         </a>
     </div>
-    <?php endif; ?>
+    <?php else:
+        header('Location: ../pages/needLog/Calendrier.php');
+    endif; ?>
 </body>
 
 </html>
