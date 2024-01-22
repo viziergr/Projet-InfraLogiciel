4a665f261f81d6468c92c3b87a591a3e7e6bac95
LouisFlop <louis.peculier@reseau.eseo.fr>
2024-01-22 01:25:32 +0100

    pushed

--- timefusion-web/public/scripts/gestion_permission.php
+++ timefusion-web/public/scripts/gestion_permission.php
@@ -2,17 +2,17 @@
 
 dd('ok');
 
-if($_SERVER['REQUEST_METHOD'] === 'POST'&& isset($_POST['promote']) && isset($_POST['member_id']) && isset($_POST['team_id'])) {
-    $member_id = $_POST['member_id'];
-    $teams->promote($member_id,$team_id);
-    header('Location: ../pages/insideteampanel.php?');
-    exit();
-}
-if($_SERVER['REQUEST_METHOD'] === 'POST'&& isset($_POST['relegate']) && isset($_POST['member_id']) && isset($_POST['team_id'])) {
-    $member_id = $_POST['member_id'];
-    $teams->relegate($member_id,$team_id);
-    header('Location: ../pages/insideteampanel.php?team_id='.$team_id);
-    exit();
-}
+// if($_SERVER['REQUEST_METHOD'] === 'POST'&& isset($_POST['promote']) && isset($_POST['member_id']) && isset($_POST['team_id'])) {
+//     $member_id = $_POST['member_id'];
+//     $teams->promote($member_id,$team_id);
+//     header('Location: ../pages/insideteampanel.php?');
+//     exit();
+// }
+// if($_SERVER['REQUEST_METHOD'] === 'POST'&& isset($_POST['relegate']) && isset($_POST['member_id']) && isset($_POST['team_id'])) {
+//     $member_id = $_POST['member_id'];
+//     $teams->relegate($member_id,$team_id);
+//     header('Location: ../pages/insideteampanel.php?team_id='.$team_id);
+//     exit();
+// }
 
 ?>
