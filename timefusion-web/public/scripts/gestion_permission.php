78f1f8b5f7f9610bf143f42e96d2c1062b102afc
LouisFlop <louis.peculier@reseau.eseo.fr>
2024-01-22 01:43:32 +0100

    pushed

--- timefusion-web/public/scripts/gestion_permission.php
+++ timefusion-web/public/scripts/gestion_permission.php
@@ -8,14 +8,14 @@ $teams = new TimeFusion\Team\Teams(connectDB());
 if($_SERVER['REQUEST_METHOD'] === 'POST'&& isset($_POST['promote']) && isset($_POST['member_id']) && isset($_POST['team_id'])) {
     $member_id = $_POST['member_id'];
     $team_id = $_POST['team_id'];
-    $teams->promote($member_id,$team_id);
+    $teams->promote($team_id,$member_id);
     header('Location: ../pages/needLog/insideteampanel.php?team_id='.$team_id.'');
     exit();
 }
 if($_SERVER['REQUEST_METHOD'] === 'POST'&& isset($_POST['relegate']) && isset($_POST['member_id']) && isset($_POST['team_id'])) {
     $member_id = $_POST['member_id'];
     $team_id = $_POST['team_id'];
-    $teams->relegate($member_id,$team_id);
+    $teams->relegate($team_id,$member_id);
     header('Location: ../pages/needLog/insideteampanel.php?team_id='.$team_id);
     exit();
 }
